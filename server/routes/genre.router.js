6c6b01339180ec84aa7b93f30d4673ec5cc1f623
Carlos Kelley <carloskelley@protonmail.com>
2022-07-09 22:44:09 -0500

    trying to get the genres router set up?

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require("../modules/pool");
 
-router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+router.get("/", (req, res) => {
+  const query = ` SELECT (genres.name) FROM genres JOIN movies_genres ON genres.id = movies_genres.genre_id WHERE movie_id = $1;`;
+  const values = [req.query.id];
+  pool
+    .query(query, values)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("error GETting genres", err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
